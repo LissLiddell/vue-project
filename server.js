b20facd0ee1bd284d15a48903250d1c3ea3b828b
Lisset G. Lopez Ochoa <lilopez@grupomavi.com>
2024-03-04 19:03:07 -0600

    feat:: Agregar  nodemon

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/',(req,res) => {
     res.send('Hello node API')
 })
 
+app.get('/blog',(req,res) => {
+    res.send('Hello Block my name is lisset')
+})
+
 app.listen(3000, ()=> {
     console.log('Node Api app is running on port 3000')
 })
