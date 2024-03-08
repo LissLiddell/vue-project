3514afa7763250877ff4832b24ae9cfa3d94a298
Lisset G. Lopez Ochoa <lilopez@grupomavi.com>
2024-03-07 18:59:53 -0600

    Feat:: Fetch - Get Data From DB

--- server.js
+++ server.js
@@ -16,6 +16,25 @@ app.get('/blog',(req,res) => {
     res.send('Hello Block my name is lisset')
 })
 
+app.get('/products',async(req,res)=>{
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/product/:id', async(req,res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);        
+    } catch (error) {
+        es.status(500).json({message: error.message})
+    }
+})
+
 app.post('/product',async(req,res)=>{
  try {
     const product = await Product.create(req.body)
@@ -25,7 +44,6 @@ app.post('/product',async(req,res)=>{
     res.status(500).json({message: error.message})
  }
 })
-
 
 mongoose.set('strictQuery',false)
 mongoose.
