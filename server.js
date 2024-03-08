078c7d71ff40ab5defbf0f3ae42fd5e5fed24235
Lisset G. Lopez Ochoa <lilopez@grupomavi.com>
2024-03-07 18:17:16 -0600

    Feat:: Uso de modelo para insertar en MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/product-model')
 const app = express()
+
+
+app.use(express.json())
 
 //routes
 
@@ -12,15 +16,20 @@ app.get('/blog',(req,res) => {
     res.send('Hello Block my name is lisset')
 })
 
-app.post('/product',(req,res)=>{
-    console.log(req.body);
-    res.send(req.body)
+app.post('/product',async(req,res)=>{
+ try {
+    const product = await Product.create(req.body)
+    res.status(200).json(product);
+ } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message: error.message})
+ }
 })
 
 
 mongoose.set('strictQuery',false)
 mongoose.
-connect('mongodb+srv://Admin:ZDPXetouUHOB6IeQ@test1.e4czoye.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Test1')
+connect('mongodb+srv://Admin:ZDPXetouUHOB6IeQ@test1.e4czoye.mongodb.net/?retryWrites=true&w=majority&appName=Test1')
 .then(()=> {
     console.log('connected to MongoDB')
     app.listen(3000, ()=> {
